6b616e98ed27b43bc78269ca8e38e3a713dda02f
Henrique Coelho <coelho.heriquesantos@hotmail.com>
2021-11-04 11:54:14 -0300

    Atualização das Classes

--- src/main/kotlin/Cursos.kt
+++ src/main/kotlin/Cursos.kt
@@ -1,22 +1,47 @@
-class Cursos {
+class Cursos(nomeDoCurso: String?,
+             nomeDoProfessor: String?,
+             ano: Int) {
 
-    var nomeDoCurso = ""
-    var nomeDoProfessor = ""
-    var ano = 0
-    var lista = mutableListOf<String>()
+    // '?' considera os valores nulos
+    val listaEstudante = mutableListOf<Estudante?>()
 
-    fun matricular (nome: String ){
-        println("Digite seu nome: ")
-        var nome = readLine()!!
-
-        while (true){
-            println("Para ver as opções, digite 'sim'. " +
-                    "E para sair ecerrar o acesso, digite 'nao': ")
-            val simNao = readLine()!!
-
-            if (simNao != "sim"){
-                break
+    fun matricular(estudante: Estudante?){
+        listaEstudante.add(estudante)
+        println("Estudante matriculado com sucesso!")
+    }
+    fun matricular(estudantes: Array<Estudante?>?){
+        if (estudantes != null){
+            for(i in estudantes){
+                listaEstudante.add(i)
             }
         }
+        println("Estudantes matriculados com sucesso!")
     }
+
+    fun desmatricular(estudante: Estudante){
+        if(listaEstudante.contains(estudante)){
+            listaEstudante.remove(estudante)
+            println("Estudante desmatriculado com sucesso!")
+        }else{
+            println("Esse estudante não existe na lista.")
+        }
+
+    }
+
+    fun contarEstudantes(): Int {
+        println("O número de estudantes cadastrados no curso é " +
+                "${listaEstudante.size}")
+        return 0
+    }
+
+    fun melhorNota(){
+        var melhorNota = listaEstudante[0]?.nota
+        for(i in listaEstudante){
+            if(i?.nota!! > melhorNota!!){
+                melhorNota = i.nota
+            }
+        }
+        println("A maior nota desse curso é: $melhorNota")
+    }
+
 }
--- src/main/kotlin/Estudante.kt
+++ src/main/kotlin/Estudante.kt
@@ -1,44 +1,38 @@
-data class Estudante (val primeiroNome: String, var ultimoNome: String){
+class Estudante (var nome: String?,
+                 var sobrenome: String?,
+                 var ra: Int) {
 
-    var registro = ""
-    var ano = 0
     var nota = 0.0
+    var ano = 0
 
-    constructor(primeiroNome: String, ultimoNome: String, ano: Int):
-            this(primeiroNome, ultimoNome){
-                this.ano = ano
+    constructor(nome: String?, sobrenome: String?,
+                ra: Int, nota: Double): this(nome, sobrenome, ra){
+                    this.nota = nota
+                }
+
+    constructor(nome: String?, sobrenome: String?,
+                ra: Int, nota: Double, ano: Int): this(nome, sobrenome, ra, nota){
+                    this.ano = ano
+                }
+
+
+    fun nomeCompleto() {
+        println("$nome $sobrenome")
     }
 
-    constructor(primeiroNome: String, ultimoNome: String, ano: Int, registro: String):
-            this(primeiroNome, ultimoNome, ano){
-                this.registro = registro
-    }
-
-
-    fun printNomeCompleto() {
-        println("$primeiroNome + $ultimoNome")
-    }
-
-    fun checarNota(){
-            val Aprovado: Unit
-            if (nota >= 60) {
-                println("Parabéns, você passou de ano!")
-                ano += 1
-            }else if (nota < 60){
-                println("Você reprovou de ano!")
-                ano -= 1
-            }
-    }
-
-
-    fun passarDeAno(): Int {
-            //TODO implement: the student should advance to the next year if he/she grade is >= 60
-            //TODO implement: o aluno deve avançar para o próximo ano se a nota for >= 60
-            // Make year = year + 1, and print "Congragulations" if the student has been approved
-            // Faça ano = ano + 1, e imprima "Parabéns" se o aluno foi aprovado
-            return 0
+    val aprovado: Boolean
+        get() {
+            return nota >= 60
         }
 
+    fun mudarAnoSeAprovado(){
 
+        if(aprovado) {
+            ano += 1
+            println("Parabéns, você foi aprovado!")
+        }else{
+            println("Você foi reprovado! Não passará de ano.")
+        }
+    }
 
 }
